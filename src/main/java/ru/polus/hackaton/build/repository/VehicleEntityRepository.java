7eb9253b414f26ab45b28deda0aa963e45f21ea2
Tuhvatullin Bulat <94221495+tuhvbulat@users.noreply.github.com>
2022-10-15 13:35:32 +0300

    super code (#1)
    Co-authored-by: tuhvbulat <tuhvbulat@mts.ru>

--- src/main/java/ru/polus/hackaton/build/repository/VehicleEntityRepository.java
+++ src/main/java/ru/polus/hackaton/build/repository/VehicleEntityRepository.java
@@ -3,7 +3,10 @@ package ru.polus.hackaton.build.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.polus.hackaton.build.model.VehicleEntity;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface VehicleEntityRepository extends JpaRepository<VehicleEntity, Long> {
+
+    List<VehicleEntity> getAllByCharacteristic(String characteristic);
+    List<VehicleEntity> getAllByCharacteristicAndModel(String characteristic, String model);
 }
