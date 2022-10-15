614758bb2d99297b3a86940f9d3661df3f2a5f13
Ildar Bagmanov <ildarb@amdocs.com>
2022-10-15 19:15:19 +0300

    add executor jobs

---
+++ src/main/java/ru/polus/hackaton/build/controller/ExecutorController.java
@@ -0,0 +1,26 @@
+package ru.polus.hackaton.build.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.polus.hackaton.build.model.Executor;
+import ru.polus.hackaton.build.repository.ExecutorRepository;
+import ru.polus.hackaton.build.repository.JobRepository;
+
+@RequestMapping("executor")
+@RestController
+public class ExecutorController {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @GetMapping("jobs/{executorId}")
+    public ResponseEntity<?> getJobsByExecutorId(@PathVariable Long executorId) {
+        return ResponseEntity.ok(jobRepository.getJobsByExecutorId(executorId));
+    }
+
+
+}
--- src/main/java/ru/polus/hackaton/build/repository/JobRepository.java
+++ src/main/java/ru/polus/hackaton/build/repository/JobRepository.java
@@ -1,6 +1,8 @@
 package ru.polus.hackaton.build.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.polus.hackaton.build.model.Customer;
 import ru.polus.hackaton.build.model.Job;
@@ -12,4 +14,7 @@ import java.util.Optional;
 public interface JobRepository extends JpaRepository<Job, Long> {
 
     Optional<List<Job>> getAllByCustomer_Id(Long customerId);
+
+    @Query("SELECT job FROM Job job WHERE job.executor.id = :executorId")
+    Optional<List<Job>> getJobsByExecutorId(@Param("executorId") Long executorId);
 }
